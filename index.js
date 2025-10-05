29f90955b5f913f9bad8866179a16f60fa34f9a6
Taha Zaib <tahazaib1222@gmail.com>
2025-10-04 17:48:59 -0700

    third commit - fixed the MongoDB connection

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', 'https://your-frontend-app.vercel.app');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
